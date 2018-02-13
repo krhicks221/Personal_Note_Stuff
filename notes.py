b4e5c4754e411bd5e9a886bfb86c87194df71fcd
Kaytlyn <kaytierose2000@yahoo.com>
2018-02-13 14:42:38 -0500

    Notes

--- notes.py
+++ notes.py
@@ -33,3 +33,75 @@ output = ["Jack", "Logan"]
 
 #list only stored once
 #losers and winners pointing at the same thing
+
+merge_sort.py
+
+
+Logan:
+merge_sort([0,5,3,2,2])
+collection = [0,2,2,3,5]
+length = 5
+midpoint = 2
+left_half = [0,5]
+right_half = [3,2,2]
+i = 2
+j = 3
+k = 5
+left_length = 2
+right_length = 3
+
+Emmalee:
+merge_sort = [0,5]
+collection = [0,5]
+length = 2
+midpoint = 1
+left_half = [0]
+right_half = [5]
+i = 1
+j = 1
+k = 2
+left_length = 1
+right_length = 1
+collectionK = [0,5]^^
+
+Brett:
+merge_sort = [(0)]
+collection = [0]
+length = 1
+
+Kaytlyn:
+merge_sort = [(5)]
+collection = [5]
+length = 1
+
+Lynzie:
+merge_sort = [(3,2,2)]
+collection = [2,2,2]
+length = 3
+midpoint = 1
+left_half = [2]
+right_half = [2,2]
+i = 1
+j = 2
+k = 3
+left_length = 1
+right_length = 2
+collection0 = right_half(0)^^
+collection2 = right_length(2)
+
+Jack:
+merge_sort = [(3)]
+collection = [3]
+length = 1
+merge_sort = [(2,2)]
+collection = [2,2]
+length = 2
+midpoint = 1
+left_half = [2]
+right_half = [2]
+i = 1
+j = 1
+k = 2
+left_length = 1
+right_length = 1
+collection1 = left_half(0)^^
